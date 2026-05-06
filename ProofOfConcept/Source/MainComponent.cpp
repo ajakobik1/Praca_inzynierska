e50988b28fdc5c096592bb06ab13d849a34a6351
ola <aleksandra.8753@gmail.com>
2026-05-06 19:28:53 +0200

    Added the fallout mod template

--- ProofOfConcept/Source/MainComponent.cpp
+++ ProofOfConcept/Source/MainComponent.cpp
@@ -5,19 +5,31 @@ MainComponent::MainComponent()
 {
     // Make sure you set the size of the component after
     // you add any child components.
-    setSize (800, 600);
+    setSize(800, 600);
+
+    if (!connect(9000)) {
+        juce::Logger::outputDebugString("BLAD: Nie udalo sie otworzyc portu 9000!");
+    }
+    else {
+        juce::Logger::outputDebugString("SUKCES: Port 9000 otwarty poprawnie!");
+    }
+
+    addListener(this, "/fallout/player/rotation");
+
+    // sender.connect("127.0.0.1", 9000); 
+    // startTimer(500);
 
     // Some platforms require permissions to open input channels so request that here
-    if (juce::RuntimePermissions::isRequired (juce::RuntimePermissions::recordAudio)
-        && ! juce::RuntimePermissions::isGranted (juce::RuntimePermissions::recordAudio))
+    if (juce::RuntimePermissions::isRequired(juce::RuntimePermissions::recordAudio)
+        && !juce::RuntimePermissions::isGranted(juce::RuntimePermissions::recordAudio))
     {
-        juce::RuntimePermissions::request (juce::RuntimePermissions::recordAudio,
-                                           [&] (bool granted) { setAudioChannels (granted ? 2 : 0, 2); });
+        juce::RuntimePermissions::request(juce::RuntimePermissions::recordAudio,
+            [&](bool granted) { setAudioChannels(granted ? 2 : 0, 2); });
     }
     else
     {
         // Specify the number of input and output channels that we want to open
-        setAudioChannels (2, 2);
+        setAudioChannels(2, 2);
     }
 }
 
@@ -28,65 +40,100 @@ MainComponent::~MainComponent()
 }
 
 //==============================================================================
-void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate)
+void MainComponent::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
 {
-    // This function will be called when the audio device is started, or when
-    // its settings (i.e. sample rate, block size, etc) are changed.
-
-    // You can use this function to initialise any resources you might need,
-    // but be careful - it will be called on the audio thread, not the GUI thread.
-
-    // For more details, see the help for AudioProcessor::prepareToPlay()
-	currentSampleRate = float(sampleRate);
-	updatePhaseDelta();
+    currentSampleRate = float(sampleRate);
+    updatePhaseDelta();
 }
 
-void MainComponent::getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill)
+void MainComponent::getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill)
 {
-    // Your audio-processing code goes here!
+    auto* leftBuffer = bufferToFill.buffer->getWritePointer(0, bufferToFill.startSample);
+    auto* rightBuffer = bufferToFill.buffer->getWritePointer(1, bufferToFill.startSample);
 
-    // For more details, see the help for AudioProcessor::getNextAudioBlock()
-
-    // Right now we are not producing any data, in which case we need to clear the buffer
-    // (to prevent the output of random noise)
-    //bufferToFill.clearActiveBufferRegion();
-
-	auto* leftBuffer = bufferToFill.buffer->getWritePointer(0, bufferToFill.startSample);
-	auto* rightBuffer = bufferToFill.buffer->getWritePointer(1, bufferToFill.startSample);
+    // --- ZABEZPIECZENIE PRZED "ZAWIESZONĄ NUTĄ" ---
+    // Zliczamy próbki. Jeśli gra nic nie wyśle przez ponad 100ms, wymuszamy ciszę.
+    samplesWithoutMessage += bufferToFill.numSamples;
+    if (samplesWithoutMessage > currentSampleRate * 0.1f) {
+        targetVolume = 0.0f;
+    }
 
     for (auto sample = 0; sample < bufferToFill.numSamples; ++sample)
     {
-		float sampleValue = std::sin(currentPhase) * 0.1f; // Sine wave at 10% volume
-		leftBuffer[sample] = sampleValue;
-		rightBuffer[sample] = sampleValue;
-		currentPhase += phaseDelta;
+        // --- PROSTY SYSTEM ADSR (Attack / Decay) ---
+        // Płynnie zbliżamy currentVolume do targetVolume, żeby nie było trzasków
+        if (currentVolume < targetVolume) {
+            currentVolume += 0.001f; // Szybki atak
+        }
+        else if (currentVolume > targetVolume) {
+            currentVolume -= 0.00005f; // Powolne wybrzmienie ("ogon")
+            if (currentVolume < 0.0f) currentVolume = 0.0f; // Zabezpieczenie przed ujemną głośnością
+        }
+        // -------------------------------------------
 
-		// Wrap the phase to prevent it from getting too large
+        // Mnożymy sinusoidę przez naszą dynamiczną głośność
+        float sampleValue = std::sin(currentPhase) * currentVolume;
+
+        leftBuffer[sample] = sampleValue;
+        rightBuffer[sample] = sampleValue;
+        currentPhase += phaseDelta;
+
         if (currentPhase >= 2.0f * juce::MathConstants<float>::pi)
-			currentPhase -= 2.0f * juce::MathConstants<float>::pi;
+            currentPhase -= 2.0f * juce::MathConstants<float>::pi;
     }
 }
 
 void MainComponent::releaseResources()
 {
-    // This will be called when the audio device stops, or when it is being
-    // restarted due to a setting change.
-
-    // For more details, see the help for AudioProcessor::releaseResources()
 }
 
 //==============================================================================
-void MainComponent::paint (juce::Graphics& g)
+void MainComponent::paint(juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    // You can add your drawing code here!
+    g.fillAll(getLookAndFeel().findColour(juce::ResizableWindow::backgroundColourId));
 }
 
 void MainComponent::resized()
 {
-    // This is called when the MainContentComponent is resized.
-    // If you add any child components, this is where you should
-    // update their positions.
+}
+
+//==============================================================================
+void MainComponent::oscMessageReceived(const juce::OSCMessage& message)
+{
+    if (message.size() == 1 && message[0].isFloat32())
+    {
+        float incomingData = message[0].getFloat32();
+
+        juce::Logger::outputDebugString("ROTACJA Z GRY: " + juce::String(incomingData));
+
+        // Porównujemy starą klatkę z nową. Ruch myszką powoduje zmianę rotacji.
+        if (std::abs(incomingData - lastRotation) > 0.001f)
+        {
+            targetVolume = 0.5f; // Włączamy głośność na 50%
+
+            // Prawdziwa rotacja to zazwyczaj przedział 0 - 6.28 (radiany). 
+            // Mapujemy to na częstotliwość od 200 Hz do ok. 1140 Hz.
+            frequency = 200.0f + (incomingData * 150.0f);
+
+            updatePhaseDelta();
+        }
+        else
+        {
+            targetVolume = 0.0f; // Postać stoi w miejscu = cisza
+        }
+
+        lastRotation = incomingData;
+        samplesWithoutMessage = 0; // RESETUJEMY BEZPIECZNIK, bo dotarł nowy pakiet!
+    }
+}
+
+//==============================================================================
+void MainComponent::timerCallback()
+{
+    // Testowy timer (wyłączony w konstruktorze)
+    sender.send("/fallout/player/rotation", testHealth);
+
+    testHealth -= 10.0f;
+    if (testHealth < 0.0f)
+        testHealth = 100.0f;
 }
--- ProofOfConcept/Source/MainComponent.h
+++ ProofOfConcept/Source/MainComponent.h
@@ -7,7 +7,10 @@
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent  : public juce::AudioAppComponent
+class MainComponent : public juce::AudioAppComponent,
+                      private juce::OSCReceiver, // Inherit from OSCReceiver to be able to listen to Open Sound Control messages
+                      private juce::OSCReceiver::ListenerWithOSCAddress<juce::OSCReceiver::MessageLoopCallback>,
+                      private juce::Timer
 {
 public:
     //==============================================================================
@@ -29,6 +32,11 @@ private:
 	float phaseDelta = 0.0f; // How much to advance the phase for each sample
 	float frequency = 440.0f; // Frequency of the sine wave
 	float currentSampleRate = 44100.0f; // Sample rate of the audio device
+    float lastRotation = 0.0f;
+    float currentVolume = 0.0f;
+    float targetVolume = 0.0f;
+    int samplesWithoutMessage = 0;
+	void oscMessageReceived(const juce::OSCMessage& message) override; // This will be called when an OSC message is received that matches the address pattern we registered for
 
     // How many cycles of the wave we go through per sample
     void updatePhaseDelta()
@@ -36,6 +44,11 @@ private:
 		auto cyclesPerSample = frequency / currentSampleRate;
 		phaseDelta = cyclesPerSample * 2.0f * juce::MathConstants<float>::pi;
 	}
+
+    void timerCallback() override;
+	juce::OSCSender sender; // This will be used to send OSC messages 
+	float testHealth = 100.0f; // Health value for testing
+    
 
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
